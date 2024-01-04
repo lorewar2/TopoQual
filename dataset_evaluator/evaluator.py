8554e377454df84f268ae6b9d39f13e04d5b1ffe
minindu0 <mw8511@live.com>
2024-01-04 10:17:38 -0600

    python bam file input

--- dataset_evaluator/evaluator.py
+++ dataset_evaluator/evaluator.py
@@ -1,4 +1,5 @@
 import torch
+import sys, getopt
 import numpy as np
 import random
 import model
@@ -13,20 +14,36 @@ DATA_PATH = "./intermediate/"
 MODEL_PATH = "./dataset_evaluator/model/multi_layered_model_new.pt"
 CONTEXT_COUNT = 3
 EXTRA_COUNT = 20
+INPUT_FILE = "./sample_files/test.ccs.bam"
+OUTPUT_FILE = "read_modified.bam"
 
-def main():
+def main(argv):
+    # get arguments input file and output file
+    inputfile = ''
+    outputfile = ''
+    opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('evaluate.py -i <inputbam> -o <outputbam>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print ('Input bam is ', inputfile)
+    print ('Output bam is ', outputfile)
     # set the seed
     torch.manual_seed(1)
     random.seed(3)
     np.random.seed(2)
     # get the arguments read bam and output bam paths
-    create_modified_bam()
+    create_modified_bam(inputfile, outputfile)
     return
 
-def create_modified_bam():
-    infile = pysam.AlignmentFile("./sample_files/test.ccs.bam", "rb", check_sq = False)
-    outfile = pysam.AlignmentFile("read_modified.bam", "wb", template = infile)
-    for read in infile:
+def create_modified_bam(inputfile, outputfile):
+    inbam = pysam.AlignmentFile(inputfile, "rb", check_sq = False)
+    outbam = pysam.AlignmentFile(outputfile, "wb", template = inbam)
+    for read in inbam:
         required_query_name = read.query_name
         # find the query in the intermediate folder
         required_path = "{}{}".format(DATA_PATH, required_query_name.replace("/", "."))
@@ -39,7 +56,7 @@ def create_modified_bam():
         # save the sequence and qualities to the new bam
         read.query_sequence = polished_seq
         read.query_qualities = pysam.qualitystring_to_array(polished_qual)
-        outfile.write(read)
+        outbam.write(read)
     return
 
 # this function will evalute the model and return the sequence and the quality scores
@@ -89,4 +106,4 @@ def evaluate(file_path):
     return (polished_sequence_string, polisehed_quality_string)
 
 if __name__ == "__main__":  
-    main()
+    main(sys.argv[1:])
